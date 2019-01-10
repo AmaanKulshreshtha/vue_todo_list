621639a44b4bf6ad34bf4fbb4d05f16827789a2e
Amaan Kulshreshtha <amaan.kulshreshtha@gmail.com>
2019-01-11 00:12:48 +0400

    Initial commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="todos">
+		<ul v-bind:key="todo.userId" v-for="todo in todos">
+			<li>{{ todo.title }}</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Todos',
+	props: ['todos']
+};
+</script>
+
+<style scoped></style>
